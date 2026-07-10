70a427334c728ed02a85a23bd428567d792098cc
fenabaars <ferna.sotoa@duocuc.cl>
2026-07-10 03:41:59 -0400

    corrigiendo errores de codigo repository

--- microservicio-envio/src/main/java/com/perfulandia/envio/repositories/EnvioRepository.java
+++ microservicio-envio/src/main/java/com/perfulandia/envio/repositories/EnvioRepository.java
@@ -2,12 +2,10 @@ package com.perfulandia.envio.repositories;
 
 import com.perfulandia.envio.models.entities.Envio;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface EnvioRepository extends JpaRepository<Envio, Long> {
 
     // H22: Listar envíos por estado (ej: PREPARACION para ver pedidos en bodega)
--- microservicio-facturacion/src/main/java/com/perfulandia/facturacion/repositories/BoletaRepository.java
+++ microservicio-facturacion/src/main/java/com/perfulandia/facturacion/repositories/BoletaRepository.java
@@ -2,12 +2,10 @@ package com.perfulandia.facturacion.repositories;
 
 import com.perfulandia.facturacion.models.entities.Boleta;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
 public interface BoletaRepository extends JpaRepository<Boleta, Long> {
 
     // H21: Listar boletas del cliente para descarga desde perfil
